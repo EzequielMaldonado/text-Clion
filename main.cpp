d4fc02f992be7ba86311ee1b064f14dcbe9a1bfc
EzequielMaldonado <ezecba22@gmail.com>
2016-03-14 19:31:47 -0300

    V 0.1

--- main.cpp
+++ main.cpp
@@ -2,8 +2,25 @@
 
 using namespace std;
 
+int potencia(int base, int exponente)
+{
+    if(exponente == 0) return 1;
+    if (exponente == 1) return base;
+    else{
+        return base*potencia(base, exponente-1);
+    }
+
+
+}
+
 int main() {
-    cout << "Hello, World!" << endl;
-    cout << "Hello, World Again!" << endl;
+
+        int base, exponente;
+    cout <<"ingrese la base:"<< endl;
+    cin >> base;
+    cout <<"ingrese el exponente: "<< endl;
+    cin >> exponente;
+    cout << "El resultado es: " << potencia(base, exponente) << endl ;
     return 0;
 }
+
